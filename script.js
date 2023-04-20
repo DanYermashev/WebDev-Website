f1b003234d0fab167f252e2b1cdbe8adc6830984
Daniel Yermashev <122382356+DanYermashev@users.noreply.github.com>
2023-04-20 12:40:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const hiringRadio = document.querySelectorAll('[name="about"]');
+const hourlyRateDiv = document.getElementById('hourly-rate');
+const hourlyRateInput = document.getElementById('hourly-rate-input');
+
+// add an event listener to detect when the radio button selection change
+hiringRadio.forEach((radio) => {
+  radio.addEventListener('change', (event) => {
+    if (event.target.value === "hiring") {
+      hourlyRateDiv.style.display = 'block';
+      hourlyRateInput.required = true;
+    } else {
+      hourlyRateDiv.style.display = 'none';
+      hourlyRateInput.required = false;
+    }
+  });
+});
+function downloadFile() {
+    window.open("C:/Users/playa/OneDrive/Desktop/WebDev/WEB-FINAL/Resume-Daniel-Yermashev.pdf")
+ }
+
+ const form = document.getElementById("contact-form");
+ const submitBtn = document.getElementById("submit-btn");
+
+ submitBtn.addEventListener("click", (event) => {
+   event.preventDefault();
+   if (form.checkValidity()) {
+     const formData = new FormData(form);
+     fetch(form.action, {
+       method: form.method,
+       body: formData
+     })
+     .then(response => response.json())
+     .then(data => console.log(data))
+     .catch(error => console.error(error));
+   } else {
+     form.reportValidity();
+   }
+   
+ });
+ 
+
+ 
